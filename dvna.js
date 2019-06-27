d62175e5acf86626393194748fdfd60ddcad32d9
Ken McDonald <kmcdon83@gmail.com>
2019-06-27 10:24:10 -0400

    Update dvna.js

--- dvna.js
+++ dvna.js
@@ -13,6 +13,7 @@ var dvna = express();
 
 var port = process.env.PORT || 3000;
 
+
 // Setup the vulnerability file path
 var vulnerabilities = [], vulnerabilities_path = './vulnerabilities/';
 filesystem.readdir(vulnerabilities_path, function (error, folders) {
